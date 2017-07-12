5616eaebb39c2afff6bfff34a0b140b361a4e4dd
ZHUZIYI <zhuziyi1989@gmail.com>
2017-07-12 17:36:15 +0800

    新增常用工具类

--- src/modules/Main/saga.js
+++ src/modules/Main/saga.js
@@ -2,8 +2,9 @@ import { call, put, takeLatest, fork, all } from 'redux-saga/effects'
 import * as homeType from '../Home/duck'
 import * as mainType from './duck'
 import { getUsers } from '../../services/user.api'
+import { axis } from '../../utils'
 
-
+console.log('数据类型---->>>>>>>',axis(999))
 
 // 初始化APP
 function* initApp() {
@@ -11,7 +12,7 @@ function* initApp() {
     const res = yield call(getUsers)
     //类似 call(fn, ...args)，但支持为 fn 指定 this 上下文。用于调用对象的方法。
     const users = res.data.data
-    console.log(users);
+    console.log('Users---->>>>>>>',users);
     const newUsers = users.map(user => {
       return ({
         id: user.id,
@@ -20,7 +21,6 @@ function* initApp() {
         following: false,
       })
     })
-  
     yield put({
       type: homeType.GET_USERS,
       payload: newUsers
---
+++ src/utils/index.js
@@ -0,0 +1,2 @@
+// JavaScript 数据类型检测
+export const axis  = elem => Object.prototype.toString.call(elem).slice(8, -1);
