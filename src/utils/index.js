a402fca349b56562b03027852e31bc2fa5dabc52
ZHUZIYI <zhuziyi1989@gmail.com>
2017-07-13 11:26:23 +0800

    忽略 .expo

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,6 @@
 // JavaScript 数据类型检测
 export const axis  = elem => Object.prototype.toString.call(elem).slice(8, -1);
+let str = 'hahay'
+console.log(str.includes('y')) // true
+let s = 'he'
+console.log(s.repeat(3)) // 'hehehe'
